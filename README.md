493b186d130ec90ce61e2627fc7852c171417268
Josh Vlk <josh@vlkpack.com>
2024-01-29 16:27:08 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# thejoshderocher.com
---
+++ app/root.jsx
@@ -0,0 +1,28 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+} from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html>
+      <head>
+        <link
+          rel="icon"
+          href="data:image/x-icon;base64,AA"
+        />
+        <Meta />
+        <Links />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
+      </head>
+      <body>
+        <h1>Hello world!</h1>
+        <Outlet />
+
+        <Scripts />
+      </body>
+    </html>
+  );
+}
