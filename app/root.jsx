c540a191c04c36a56431704495f5aa7e26dcf215
Josh Vlk <josh@vlkpack.com>
2024-01-30 08:26:25 -0500

    perf, best practices, and seo

--- app/root.jsx
+++ app/root.jsx
@@ -15,10 +15,19 @@ export const links = () => [
   { rel: "stylesheet", href: styles },
 ];
 
+export const meta = () => [
+  {name: "viewport", content: "width=device-width, initial-scale=1" },
+  {title: "The Josh Derocher" },
+  {
+      name: "description",
+      content: "Blog and all about Josh Derocher.",
+    },
+]
+
 export default function App() {
 
   return (
-    <html>
+    <html lang="en">
       <head>
         <link
           rel="icon"
@@ -26,10 +35,11 @@ export default function App() {
         />
         <Meta />
         <Links />
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       </head>
-      <body className="container">
-        <Outlet />
+      <body>
+        <main>
+          <Outlet />
+        </main>
         <Scripts />
         <LiveReload/>
       </body>
