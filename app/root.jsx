8cbc4ec91f0826655a3b2b1fc415fa80d619a765
Josh Vlk <josh@vlkpack.com>
2024-01-29 22:18:08 -0500

    dummy data to test out loader

--- app/root.jsx
+++ app/root.jsx
@@ -3,9 +3,17 @@ import {
   Meta,
   Outlet,
   Scripts,
+  json,
+  useLoaderData
 } from "@remix-run/react";
 
+export const loader = async () => {
+  return json({ data: await fetch("https://baconipsum.com/api/?type=meat-and-filler").then(res => res.json())})
+}
+
 export default function App() {
+  const {data} = useLoaderData()
+
   return (
     <html>
       <head>
@@ -17,8 +25,9 @@ export default function App() {
         <Links />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
       </head>
-      <body>
+      <body className="container">
         <h1>Hello world!</h1>
+        {data.map(t => <p key={t}>{t}</p>)}
         <Outlet />
 
         <Scripts />
