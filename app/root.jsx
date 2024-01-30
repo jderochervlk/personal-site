9333cb1387e371e1826854f0a7d79fc961480d5c
Josh Vlk <josh@vlkpack.com>
2024-01-29 23:29:07 -0500

    inline pico

--- app/root.jsx
+++ app/root.jsx
@@ -9,6 +9,12 @@ import {
 } from "@remix-run/react";
 
 
+import styles from "../src/pico.css";
+
+export const links = () => [
+  { rel: "stylesheet", href: styles },
+];
+
 export default function App() {
 
   return (
@@ -20,7 +26,7 @@ export default function App() {
         />
         <Meta />
         <Links />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       </head>
       <body className="container">
         <Outlet />
