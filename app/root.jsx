3cf9ef74e349274cd8bc738ce7fa14902bd14e77
Josh Vlk <josh@vlkpack.com>
2024-01-29 22:28:49 -0500

    remix entry files

--- app/root.jsx
+++ app/root.jsx
@@ -11,6 +11,15 @@ export const loader = async () => {
   return json({ data: await fetch("https://baconipsum.com/api/?type=meat-and-filler").then(res => res.json())})
 }
 
+export const headers = ({
+  actionHeaders,
+  errorHeaders,
+  loaderHeaders,
+  parentHeaders,
+}) => ({
+  "Cache-Control": "max-age=300, s-maxage=3600",
+});
+
 export default function App() {
   const {data} = useLoaderData()
 
