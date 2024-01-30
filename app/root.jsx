f41dae546adba5b51d1f7a5188929d8880a38d16
Josh Vlk <josh@vlkpack.com>
2024-01-30 08:42:01 -0500

    add biome and husky

--- app/root.jsx
+++ app/root.jsx
@@ -1,48 +1,42 @@
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  json,
-  useLoaderData
-} from "@remix-run/react";
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	json,
+	useLoaderData,
+} from "@remix-run/react"
 
+import styles from "../src/pico.css"
 
-import styles from "../src/pico.css";
-
-export const links = () => [
-  { rel: "stylesheet", href: styles },
-];
+export const links = () => [{ rel: "stylesheet", href: styles }]
 
 export const meta = () => [
-  {name: "viewport", content: "width=device-width, initial-scale=1" },
-  {title: "The Josh Derocher" },
-  {
-      name: "description",
-      content: "Blog and all about Josh Derocher.",
-    },
+	{ name: "viewport", content: "width=device-width, initial-scale=1" },
+	{ title: "The Josh Derocher" },
+	{
+		name: "description",
+		content: "Blog and all about Josh Derocher.",
+	},
+	{ charset: "UTF-8" },
 ]
 
 export default function App() {
-
-  return (
-    <html lang="en">
-      <head>
-        <link
-          rel="icon"
-          href="data:image/x-icon;base64,AA"
-        />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <main>
-          <Outlet />
-        </main>
-        <Scripts />
-        <LiveReload/>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<link rel="icon" href="data:image/x-icon;base64,AA" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<main>
+					<Outlet />
+				</main>
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	)
 }
--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,26 +1,32 @@
-import { json, useLoaderData } from '@remix-run/react';
-import { make as Hero } from '../../src/home_hero.jsx'
+import { json, useLoaderData } from "@remix-run/react"
+import { make as Hero } from "../../src/home_hero.jsx"
 
 export const headers = ({
-  actionHeaders,
-  errorHeaders,
-  loaderHeaders,
-  parentHeaders,
+	actionHeaders,
+	errorHeaders,
+	loaderHeaders,
+	parentHeaders,
 }) => ({
-  "Cache-Control": "max-age=300, s-maxage=3600",
-});
+	"Cache-Control": "max-age=300, s-maxage=3600",
+})
 
 export const loader = async () => {
-  return json({ data: await fetch("https://baconipsum.com/api/?type=meat-and-filler").then(res => res.json())})
+	return json({
+		data: await fetch("https://baconipsum.com/api/?type=meat-and-filler").then(
+			(res) => res.json(),
+		),
+	})
 }
 
-export default function() {
-    const {data} = useLoaderData()
+export default function () {
+	const { data } = useLoaderData()
 
-    return (
-        <>
-        <Hero />
-        {data.map(t => <p key={t}>{t}</p>)}
-        </>
-    )
+	return (
+		<>
+			<Hero />
+			{data.map((t) => (
+				<p key={t}>{t}</p>
+			))}
+		</>
+	)
 }
